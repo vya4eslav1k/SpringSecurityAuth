057a157637d7cf9327712fcf6f1f98f30ade7e44
Maxim Slavik <krosh.das@bk.ru>
2024-06-12 22:51:50 +0300

    registration tool added

---
+++ src/main/java/ru/max/projects/SpringSecurityTrial2/Controllers/AuthenticationController.java
@@ -0,0 +1,9 @@
+package ru.max.projects.SpringSecurityTrial2.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthentificationController {
+    @GetMapping("/register")
+}
